271c91360b4e98284472a411dc86adcb7ac3c798
Ahmed Hassanen <ahmed180116@feng.bu.edu.eg>
2023-02-05 20:52:00 +0200

    edit

--- users/src/controllers/authController.js
+++ users/src/controllers/authController.js
@@ -56,16 +56,16 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     return next(new AppError("there is no user with email address", 404));
   }
 
-  const resetToken = user.createPasswordResetToken();
+  const verifyCode = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
 
-  const message = `forgot your Password? your reset password code is 
-  ${resetToken}`;
+  const message = `forgot your Password? your reset password code is `;
   try {
     await sendEmail({
       email: user.email,
       subject: "your password reset token (Valid for 10m)",
       message,
+      verifyCode,
     });
 
     res.status(200).json({
