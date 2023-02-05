aa9a867519e17d8aa2569da278eb63f85de7712f
Ahmed Hassanen <ahmed180116@feng.bu.edu.eg>
2023-02-05 20:31:37 +0200

    add nginx config

--- users/src/controllers/errorController.js
+++ users/src/controllers/errorController.js
@@ -1,4 +1,4 @@
-const AppError = require('../utils/appError');
+const AppError = require("../utils/appError");
 
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}.`;
@@ -16,36 +16,29 @@ const handleDuplicateFieldsDB = (err) => {
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
-  const message = `Invalid input data. ${errors.join('. ')}`;
+  const message = `Invalid input data. ${errors.join(". ")}`;
   return new AppError(message, 400);
 };
 
 const handleJWTError = () =>
-  new AppError('Invalid token. Please log in again!', 401);
+  new AppError("Invalid token. Please log in again!", 401);
 
 const handleJWTExpiredError = () =>
-  new AppError('Your token has expired! Please log in again.', 401);
+  new AppError("Your token has expired! Please log in again.", 401);
 
 const sendErrorDev = (err, req, res) => {
   //API
-  if (req.originalUrl.startsWith('/api')) {
-    res.status(err.statusCode).json({
-      status: err.status,
-      error: err,
-      message: err.message,
-      stack: err.stack,
-    });
-  } else {
-    //RENDERD WEB SITE
-    res.status(err.statusCode).render('error', {
-      title: 'Something went wrong',
-      msg: err.message,
-    });
-  }
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
 };
 
 const sendErrorProd = (err, req, res) => {
-  if (req.originalUrl.startsWith('/api')) {
+  if (req.originalUrl.startsWith("/api")) {
     // Operational, trusted error: send message to client
     if (err.isOperational) {
       return res.status(err.statusCode).json({
@@ -60,14 +53,14 @@ const sendErrorProd = (err, req, res) => {
 
     // 2) Send generic message
     return res.status(500).json({
-      status: 'error',
-      message: 'Something went very wrong!',
+      status: "error",
+      message: "Something went very wrong!",
     });
   }
   if (err.isOperational) {
     //RENDERD WEB SITE
-    return res.status(err.statusCode).render('error', {
-      title: 'Something went wrong',
+    return res.status(err.statusCode).render("error", {
+      title: "Something went wrong",
       msg: err.message,
     });
 
@@ -75,9 +68,9 @@ const sendErrorProd = (err, req, res) => {
   }
 
   // 2) Send generic message
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong',
-    msg: 'please try again later',
+  return res.status(err.statusCode).render("error", {
+    title: "Something went wrong",
+    msg: "please try again later",
   });
 };
 
@@ -85,20 +78,20 @@ module.exports = (err, req, res, next) => {
   // .log(err.stack);
 
   err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+  err.status = err.status || "error";
 
-  if (process.env.NODE_ENV === 'development') {
+  if (process.env.NODE_ENV === "development") {
     sendErrorDev(err, req, res);
-  } else if (process.env.NODE_ENV === 'production') {
+  } else if (process.env.NODE_ENV === "production") {
     let error = { ...err };
     error.name = err.name;
     error.message = err.message;
-    if (error.name === 'CastError') error = handleCastErrorDB(error);
+    if (error.name === "CastError") error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
-    if (error.name === 'ValidationError')
+    if (error.name === "ValidationError")
       error = handleValidationErrorDB(error);
-    if (error.name === 'JsonWebTokenError') error = handleJWTError();
-    if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+    if (error.name === "JsonWebTokenError") error = handleJWTError();
+    if (error.name === "TokenExpiredError") error = handleJWTExpiredError();
 
     sendErrorProd(error, req, res);
   }
