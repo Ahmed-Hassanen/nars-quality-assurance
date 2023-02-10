557d0d5c76f541fddfab6dca066aea32ef7f20f4
Ahmed Hassanen <66378642+Ahmed-Hassanen@users.noreply.github.com>
2023-02-10 13:25:20 +0200

    Update studentModel.js

--- users/src/models/studentModel.js
+++ users/src/models/studentModel.js
@@ -48,15 +48,15 @@ const studentSchema = new mongoose.Schema({
     select: false,
   },
 
-  about: {
-    type: String,
-  },
   courses: [String],
   passedCourses: [String],
   program: String,
   faculty: String,
   department: String,
-  academicYear: String,
+  academicYear: {
+    type: String,
+    required: [true, "student must belong to academic year"],
+                },
   role: String,
 });
 
