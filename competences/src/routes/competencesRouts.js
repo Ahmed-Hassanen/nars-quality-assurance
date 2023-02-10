fc8b6e98bbf0f3df682ca706348ac86c703b3dcd
Ahmed Hassanen <ahmed180116@feng.bu.edu.eg>
2023-02-10 15:00:41 +0200

    connect department and faculty with competences

--- competences/src/routes/competencesRouts.js
+++ competences/src/routes/competencesRouts.js
@@ -2,9 +2,9 @@ const express = require("express");
 const competencesController = require("../controllers/competencesController");
 
 const router = express.Router();
-router.get('/',competencesController.getAll)
-router.post('/',competencesController.addCompetences)
-router.patch('/:id',competencesController.updateCompetences)
-router.delete('/:id',competencesController.deleteOne)
+router.post("/all", competencesController.getAll);
+router.post("/", competencesController.addCompetences);
+router.patch("/:id", competencesController.updateCompetences);
+router.delete("/:id", competencesController.deleteOne);
 
-module.exports =router;
+module.exports = router;
--- faculty/src/controllers/facultyController.js
+++ faculty/src/controllers/facultyController.js
@@ -8,7 +8,34 @@ const faculty = require("../models/facultyModel");
 exports.getAllFaculties = factory.getAll(Faculty);
 exports.deletefaculty = factory.deleteOne(Faculty);
 exports.updateFaculty = factory.updateOne(Faculty);
-exports.createFaculty = factory.createOne(Faculty);
+exports.createFaculty = catchAsync(async (req, res, next) => {
+  if (!req.body.competences) {
+    return next(new AppError("faculty must have competences", 400));
+  }
+  const competences_ids = await axios
+    .post(`http://competences:8080/`, {
+      competences: req.body.competences,
+      code: "a",
+    })
+    .then((res) => res.data)
+    .catch((e) => {
+      return {
+        status: false,
+        message: "something went wrong",
+        code: 500,
+      };
+    });
+  if (competences_ids.status === false) {
+    return next(new AppError(competences_ids.message, competences_ids.code));
+  }
+  req.body.competences = competences_ids.ids;
+  const doc = await Faculty.create(req.body);
+  res.status(201).json({
+    status: "success",
+
+    data: doc,
+  });
+});
 exports.getFacultySummary = factory.getOne(Faculty);
 exports.getFaculty = catchAsync(async (req, res, next) => {
   const header = `authorization: Bearer ${req.cookies.jwt}`;
@@ -27,6 +54,7 @@ exports.getFaculty = catchAsync(async (req, res, next) => {
   if (departments.status === false) {
     return next(new AppError(departments.message, departments.code));
   }
+
   const departmentsNames = [];
 
   departments.data.forEach((department) => {
@@ -40,10 +68,72 @@ exports.getFaculty = catchAsync(async (req, res, next) => {
     return next(new AppError("No document found with that id", 404));
   }
   doc.departments = departmentsNames;
+
+  const competences_description = await axios
+    .post(`http://competences:8080/all/`, { ids: doc.competences })
+    .then((res) => res.data)
+    .catch((e) => {
+      return {
+        status: false,
+        message: "something went wrong",
+        code: 500,
+      };
+    });
+  if (competences_description.status === false) {
+    return next(
+      new AppError(
+        competences_description.message,
+        competences_description.code
+      )
+    );
+  }
+  doc.competences = [];
+  competences_description.data.forEach((competence) => {
+    doc.competences.push(competence.description);
+  });
+  console.log(doc.competences);
   //console.log(doc.departments);
   res.status(200).json({
     status: "success",
 
     data: doc,
   });
+});
+
+exports.updateCompetences = catchAsync(async (req, res, next) => {
+  let query = Faculty.findById(req.params.id);
+  const doc = await query;
+
+  if (!doc) {
+    return next(new AppError("No document found with that id", 404));
+  }
+  if (!req.body.code) {
+    return next(new AppError("should specify code", 400));
+  }
+  if (!req.body.description) {
+    return next(new AppError("should specify description", 400));
+  }
+  competence_id = doc.competences[parseInt(req.body.code[1]) - 1];
+  //console.log(competence_id);
+  const competences_description = await axios
+    .patch(`http://competences:8080/${competence_id}`, {
+      description: req.body.description,
+    })
+    .then((res) => res.data)
+    .catch((e) => {
+      return {
+        status: false,
+        message: "something went wrong",
+        code: 500,
+      };
+    });
+  if (competences_description.status === false) {
+    return next(
+      new AppError(
+        competences_description.message,
+        competences_description.code
+      )
+    );
+  }
+  next();
 });
