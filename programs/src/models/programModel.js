f2a51d56640c5e16d1fd0fb335234932a49e846a
Ahmed Hassanen <ahmed180116@feng.bu.edu.eg>
2023-04-14 17:51:55 +0200

    upload and download program specs

--- programs/src/models/programModel.js
+++ programs/src/models/programModel.js
@@ -2,32 +2,34 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const programSchema = new mongoose.Schema({
-
-name:{
-    type:String,
+  name: {
+    type: String,
     required: [true, "program must have a name"],
-},
-competences:{
-    type:[
-        {
-    code:{
-     type:String,
-    required: [true, "program must have a code"],
-         },
-    description:{
-    type:String,
-    required: [true, "program must have a description"],
-               }
-    }
-       ],
+  },
+  competences: {
+    type: [
+      {
+        code: {
+          type: String,
+          required: [true, "program must have a code"],
+        },
+        description: {
+          type: String,
+          required: [true, "program must have a description"],
+        },
+      },
+    ],
     required: [true, "Competences must have a description"],
-},
+  },
 
-department:{
-    type:String,
+  department: {
+    type: String,
     required: [true, "program must have a department"],
-}
-})
+  },
+  programSpcs: {
+    type: String,
+  },
+});
 
 const program = mongoose.model("program", programSchema);
 
